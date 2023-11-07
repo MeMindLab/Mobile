ae2c1884706729b0084fc69948b50baba44123d5
Luceta <57941164+Luceta@users.noreply.github.com>
2023-11-07 19:58:51 +0900

    fix: add defaultLayout props

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -2,15 +2,40 @@ import 'package:flutter/material.dart';
 
 class DefaultLayout extends StatelessWidget {
   final Widget child;
+  final String? title;
+  final Widget? bottomNavigationBar;
+
   const DefaultLayout({
     super.key,
     required this.child,
+    this.title,
+    this.bottomNavigationBar,
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: child,
+      appBar: renderAppBar(),
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? renderAppBar() {
+    if (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        elevation: 0,
+        title: Text(
+          title!,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        foregroundColor: Colors.black,
+      );
+    }
   }
 }
