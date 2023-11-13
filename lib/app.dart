8aa5deba5fab525ea1a0ad5588d7fb5f89160bbe
윤정현 <alex_yun@yunjeonghyeon-ui-MacBookAir.local>
2023-11-13 13:11:45 +0900

    add: report chart

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:me_mind/common/theme/custom_theme.dart';
 import 'package:me_mind/common/view/on_boarding.dart';
+import 'package:me_mind/report/view/s_report.dart';
 import 'package:me_mind/user/view/signup_screen.dart';
 
 import 'common/theme/custom_theme_app.dart';
@@ -47,7 +48,7 @@ class AppState extends State<App> with WidgetsBindingObserver {
           locale: context.locale,
           title: 'Me mind',
           theme: CustomThemeHolder.of(context).theme.themeData,
-          home: const SignUpScreen(),
+          home: const Report(),
         );
       }),
     );
@@ -65,6 +66,8 @@ class AppState extends State<App> with WidgetsBindingObserver {
         App.isForeground = false;
         break;
       case AppLifecycleState.detached:
+        break;
+      case AppLifecycleState.hidden:
         break;
     }
     super.didChangeAppLifecycleState(state);
