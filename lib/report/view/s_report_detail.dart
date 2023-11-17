4a05ca7e33c29f13affd40f7a2728b22f39590ae
Luceta <57941164+Luceta@users.noreply.github.com>
2023-11-18 01:29:03 +0900

    add: report detail screen

--- lib/report/view/s_report_detail.dart
+++ lib/report/view/s_report_detail.dart
@@ -41,72 +41,78 @@ class _ReportDetailScreenState extends State<ReportDetailScreen> {
       bottomNavigationBar: const RootTab(),
       child: Column(
         children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 18.0),
-                child: Column(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: const Color(0xff2C3642),
-                      ),
-                      width: double.infinity,
-                      child: const Padding(
-                        padding: EdgeInsets.all(20),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '주간 그래프',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w400,
-                                fontSize: 20,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 16,
-                            ),
-                            AspectRatio(
-                              aspectRatio: 1.70,
-                              child: ReportChart(),
-                            ),
-                          ],
-                        ),
-                      ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: const Color(0xff2C3642),
+            ),
+            width: double.infinity,
+            child: const Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '주간 그래프',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
                     ),
-                    const Divider(
-                      height: 20,
-                      thickness: 1,
-                      color: Colors.white,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          "2023.10",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                        SvgPicture.asset('assets/svg/icon/search.svg'),
-                      ],
-                    ),
-                    const ReportCard(
-                      keywords: ["키워드1", "키워드2", "키워드3"],
-                      summary:
-                          "이곳에는 ai summary 내용이 들어가게 됩니다이곳에는 ai summary 내용이 들어가게 됩니다이곳에는 ai summa이곳에는 ai summary 내용이 들어가게 됩...",
-                      date: '2023.10.31',
-                    ),
-                  ],
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  AspectRatio(
+                    aspectRatio: 1.70,
+                    child: ReportChart(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Divider(
+            height: 20,
+            thickness: 1,
+            color: Colors.white,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "2023.10",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
                 ),
               ),
+              SvgPicture.asset('assets/svg/icon/search.svg'),
+            ],
+          ),
+          Expanded(
+            child: CustomScrollView(
+              slivers: [
+                renderReports(),
+              ],
             ),
           ),
         ],
       ),
     );
   }
+}
+
+SliverList renderReports() {
+  return SliverList(
+    delegate: SliverChildBuilderDelegate(
+      (context, builder) {
+        return const ReportCard(
+          keywords: ["키워드1", "키워드2", "키워드3"],
+          summary:
+              "이곳에는 ai summary 내용이 들어가게 됩니다이곳에는 ai summary 내용이 들어가게 됩니다이곳에는 ai summa이곳에는 ai summary 내용이 들어가게 됩...",
+          date: '2023.10.31',
+        );
+      },
+      childCount: 4,
+    ),
+  );
 }
