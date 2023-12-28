8969a709a3ea56696fc964bd44a4513da31a146a
Luceta <57941164+Luceta@users.noreply.github.com>
2023-12-28 19:01:12 +0900

    fix:signup  splash screen image

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:me_mind/chat/view/s_chat.dart';
 import 'package:me_mind/common/theme/custom_theme.dart';
 import 'package:me_mind/user/view/s_signin.dart';
+import 'package:me_mind/user/view/s_signup_welcome.dart';
 import 'common/theme/custom_theme_app.dart';
 import 'common/theme/custom_theme_holder.dart';
 
@@ -46,7 +47,7 @@ class AppState extends State<App> with WidgetsBindingObserver {
           locale: context.locale,
           title: 'Me mind',
           theme: CustomThemeHolder.of(context).theme.themeData,
-          home: const Chat(),
+          home: const SignUpWelcome(),
         );
       }),
     );
