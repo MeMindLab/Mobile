a693aeb5f1a0ac5d56feaea53dee13df6ce9bb3f
jeonhl7579 <tpgus19@gmail.com>
2024-01-25 22:08:36 +0900

    feat: 그림일기 대기 페이지

--- lib/diary/view/s_diary.dart
+++ lib/diary/view/s_diary.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:me_mind/common/layout/default_layout.dart';
+import 'package:me_mind/common/layout/topbar/widget/back_arrow.dart';
 import 'package:me_mind/common/store.dart';
 import 'package:me_mind/common/component/root_tab.dart';
+import 'package:me_mind/diary/view/f_wait.dart';
 
 class Diary extends StatefulWidget {
   const Diary({super.key});
@@ -19,9 +21,11 @@ class _DiaryState extends State<Diary> {
   @override
   Widget build(BuildContext context) {
     return const DefaultLayout(
+      title: "그림일기",
+      appBarLeading: const BackArrowLeading(),
       child: Column(
         children: [
-          Text('3'),
+          WaitFragment(),
         ],
       ),
       bottomNavigationBar: RootTab(),
