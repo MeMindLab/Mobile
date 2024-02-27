e34408dc537e829ba1157c42f3a075e707e7bd19
jeonhl7579 <tpgus19@gmail.com>
2024-02-27 18:46:23 +0900

    feat: 알림 설정 페이지 및,refactor: 세팅 페이지 수정

--- lib/settings/view/s_setting.dart
+++ lib/settings/view/s_setting.dart
@@ -1,20 +1,19 @@
-// ignore_for_file: sort_child_properties_last
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:me_mind/common/component/dialog/d_alert_dialog.dart';
 import 'package:me_mind/common/component/dialog/d_multichoice_dialog.dart';
 import 'package:me_mind/common/component/dialog/w_dialog_button.dart';
 import 'package:me_mind/common/constant/app_colors.dart';
 import 'package:me_mind/common/constant/font_sizes.dart';
 import 'package:me_mind/common/layout/default_layout.dart';
+import 'package:me_mind/common/layout/topbar/widget/back_arrow.dart';
+import 'package:me_mind/common/layout/topbar/widget/lemon_number.dart';
 import 'package:me_mind/common/store.dart';
-import 'package:me_mind/common/component/root_tab.dart';
 import 'package:me_mind/common/theme/custom_theme.dart';
 import 'package:me_mind/common/theme/custom_theme_holder.dart';
-import 'package:me_mind/common/view/on_boarding.dart';
-import 'package:me_mind/report/view/s_report_detail.dart';
 import 'package:me_mind/settings/component/certified_box.dart';
 import 'package:me_mind/settings/component/settings_menu.dart';
+import 'package:me_mind/settings/services/logout_service.dart';
+import 'package:me_mind/settings/view/s_setting_notification.dart';
 import 'package:me_mind/settings/view/s_setting_opinion.dart';
 import 'package:me_mind/settings/view/s_setting_theme.dart';
 import 'package:me_mind/settings/view/s_setting_userinfo.dart';
@@ -42,38 +41,12 @@ class _SettingState extends State<Settings> {
     return DefaultLayout(
       title: "설정",
       backgroundColor: theme.appColors.userInputBackground,
-      appBarActions: [
-        Container(
-          margin: const EdgeInsets.fromLTRB(0, 0, 20, 0),
-          child: Row(
-            children: [
-              //SvgPicture.asset('assets/svg/icon/vitamin.svg'),
-              Text('10',
-                  style: FontSizes.getHeadline1Style()
-                      .copyWith(color: theme.appColors.iconButton)),
-            ],
-          ),
-        ),
-      ],
-      appBarLeading: IconButton(
-        onPressed: () async {
-          await setBottomIdx(1);
-          Navigator.pushReplacement(
-              context,
-              PageRouteBuilder(
-                pageBuilder: ((BuildContext context,
-                        Animation<double> animation1,
-                        Animation<double> animation2) =>
-                    const ReportDetailScreen()),
-                transitionDuration: Duration.zero,
-                reverseTransitionDuration: Duration.zero,
-              ));
-        },
-        icon: const Icon(Icons.arrow_back),
-      ),
+      appBarActions: [LemonNumberWidget(numberText: "10")],
+      appBarLeading: const BackArrowLeading(),
 
       // 설정 화면 Body
       child: SingleChildScrollView(
+        physics: const NeverScrollableScrollPhysics(),
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
@@ -81,7 +54,7 @@ class _SettingState extends State<Settings> {
               Stack(children: [
                 SettingMenu(
                   subscribe: true,
-                  height: 97,
+                  height: 93,
                   content: ListTile(
                     title: Column(
                       mainAxisSize: MainAxisSize.min,
@@ -121,39 +94,46 @@ class _SettingState extends State<Settings> {
                         ),
                         onPressed: () {
                           // 구독 버튼
+                          // Navigator.push(
+                          //     context,
+                          //     MaterialPageRoute(
+                          //         builder: (context) => const SubscribePage()));
                         },
                       ),
                     ),
                   ),
                 ),
-                Positioned(
-                  child: Container(
-                    width: double.infinity,
-                    height: 97,
-                    decoration: BoxDecoration(
-                        color: const Color.fromRGBO(0, 0, 0, 0.8),
-                        borderRadius: BorderRadius.circular(13)),
-                    child: Center(
-                        child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.ios_share,
-                          color: theme.appColors.seedColor,
-                        ),
-                        Text(
-                          "1월 중 오픈 예정",
-                          style: FontSizes.getContentStyle()
-                              .copyWith(color: theme.appColors.seedColor),
-                        ),
-                      ],
-                    )),
-                  ),
-                )
+                // Positioned(
+                //   child: Container(
+                //     width: double.infinity,
+                //     height: 93,
+                //     decoration: BoxDecoration(
+                //         color: const Color.fromRGBO(0, 0, 0, 0.8),
+                //         borderRadius: BorderRadius.circular(13)),
+                //     child: Center(
+                //         child: Column(
+                //       mainAxisAlignment: MainAxisAlignment.center,
+                //       children: [
+                //         Icon(
+                //           Icons.ios_share,
+                //           color: theme.appColors.seedColor,
+                //         ),
+                //         Text(
+                //           "1월 중 오픈 예정",
+                //           style: FontSizes.getContentStyle()
+                //               .copyWith(color: theme.appColors.seedColor),
+                //         ),
+                //       ],
+                //     )),
+                //   ),
+                // )
               ]),
+              const SizedBox(
+                height: 10,
+              ),
               SettingMenu(
                 subscribe: false,
-                height: 66,
+                height: 65,
                 content: ListTile(
                   onTap: () {
                     // 계정 정보 페이지로 이동하는 부분
@@ -170,14 +150,17 @@ class _SettingState extends State<Settings> {
                         style: FontSizes.getHeadline2Style()
                             .copyWith(color: theme.appColors.iconButton),
                       ),
-                      CertifiedBox(isCertified: true),
+                      CertifiedBox(isCertified: false),
                     ],
                   ),
                 ),
               ),
+              const SizedBox(
+                height: 10,
+              ),
               SettingMenu(
                 subscribe: false,
-                height: 66,
+                height: 65,
                 content: ListTile(
                   onTap: () {
                     Navigator.pushReplacement(
@@ -192,15 +175,44 @@ class _SettingState extends State<Settings> {
                         ));
                   },
                   title: Text(
-                    "테마 설정하기",
+                    "테마 설정",
                     style: FontSizes.getHeadline2Style()
                         .copyWith(color: theme.appColors.iconButton),
                   ),
                 ),
               ),
+              const SizedBox(
+                height: 10,
+              ),
               SettingMenu(
                 subscribe: false,
-                height: 66,
+                height: 65,
+                content: ListTile(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder: ((BuildContext context,
+                                  Animation<double> animation1,
+                                  Animation<double> animation2) =>
+                              const SettingNotification()),
+                          transitionDuration: Duration.zero,
+                          reverseTransitionDuration: Duration.zero,
+                        ));
+                  },
+                  title: Text(
+                    "알림 설정",
+                    style: FontSizes.getHeadline2Style()
+                        .copyWith(color: theme.appColors.iconButton),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SettingMenu(
+                subscribe: false,
+                height: 65,
                 content: ListTile(
                   onTap: () {
                     print("의견 보내기");
@@ -216,10 +228,14 @@ class _SettingState extends State<Settings> {
                   ),
                 ),
               ),
+              const SizedBox(
+                height: 10,
+              ),
               SettingMenu(
                   subscribe: false,
                   height: 198,
                   content: ListView.separated(
+                      physics: const NeverScrollableScrollPhysics(),
                       shrinkWrap: true,
                       itemBuilder: (BuildContext contexnt, int idx) {
                         return ListTile(
@@ -236,9 +252,12 @@ class _SettingState extends State<Settings> {
                             thickness: 3,
                           ),
                       itemCount: 3)),
+              const SizedBox(
+                height: 10,
+              ),
               SettingMenu(
                 subscribe: false,
-                height: 66,
+                height: 65,
                 content: ListTile(
                     title: Text(
                       "로그아웃",
@@ -250,13 +269,15 @@ class _SettingState extends State<Settings> {
                             title: "정말 로그아웃 하시겠어요?",
                             body: "내년 1월 중 오픈 예정입니다.",
                             isRow: true,
-                            isNarrow: false,
+                            isNarrow: true,
                             actions: [
                               AlertDialogButton(
                                   theme: theme,
                                   bgColor: theme.appColors.grayButtonBackground,
-                                  content: "닫기",
+                                  content: "네",
                                   onSubmit: () {
+                                    LogoutService().logout();
+                                    // 로그인 페이지로 이동
                                     Navigator.pop(context);
                                   }),
                               AlertDialogButton(
@@ -274,7 +295,6 @@ class _SettingState extends State<Settings> {
           ),
         ),
       ),
-      bottomNavigationBar: const RootTab(),
     );
   }
 }
