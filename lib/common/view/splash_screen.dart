756283c84785df0bf36d04e5c5552fcb947a372f
jeonhl7579 <tpgus19@gmail.com>
2024-03-09 15:59:22 +0900

    feat: dio interceptor

--- lib/common/view/splash_screen.dart
+++ lib/common/view/splash_screen.dart
@@ -18,7 +18,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    // deleteToken();
+    //deleteToken();
     checkToken();
   }
 
