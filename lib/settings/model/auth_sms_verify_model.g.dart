5031bbf733d10550f3a163586bfa70ee3b78e0e5
jeonhl7579 <tpgus19@gmail.com>
2024-06-30 21:02:05 +0900

    sms,sms_verify 모델 추가

---
+++ lib/settings/model/auth_sms_verify_model.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth_sms_verify_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AuthSmsVerifyModel _$AuthSmsVerifyModelFromJson(Map<String, dynamic> json) =>
+    AuthSmsVerifyModel(
+      success: json['success'] as bool,
+      data: SmsVerifyData.fromJson(json['data'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$AuthSmsVerifyModelToJson(AuthSmsVerifyModel instance) =>
+    <String, dynamic>{
+      'success': instance.success,
+      'data': instance.data,
+    };
+
+SmsVerifyData _$SmsVerifyDataFromJson(Map<String, dynamic> json) =>
+    SmsVerifyData(
+      result:
+          SmsVerifyDataResult.fromJson(json['result'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$SmsVerifyDataToJson(SmsVerifyData instance) =>
+    <String, dynamic>{
+      'result': instance.result,
+    };
+
+SmsVerifyDataResult _$SmsVerifyDataResultFromJson(Map<String, dynamic> json) =>
+    SmsVerifyDataResult(
+      to: json['to'] as String,
+      channel: json['channel'] as String,
+      status: json['status'] as String,
+      valid: json['valid'] as bool,
+    );
+
+Map<String, dynamic> _$SmsVerifyDataResultToJson(
+        SmsVerifyDataResult instance) =>
+    <String, dynamic>{
+      'to': instance.to,
+      'channel': instance.channel,
+      'status': instance.status,
+      'valid': instance.valid,
+    };
