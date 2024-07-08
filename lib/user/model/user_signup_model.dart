4bf0dff96e866bfe08d94a26c6af1c28ef90189d
jeonhl7579 <tpgus19@gmail.com>
2024-07-08 20:17:16 +0900

    refactor: singup,signin service 수정

--- lib/user/model/user_signup_model.dart
+++ lib/user/model/user_signup_model.dart
@@ -1,13 +1,33 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_signup_model.g.dart';
+
+@JsonSerializable()
 class UserSignUpModel {
-  final String msg;
+  final String email;
+  final int id;
+  final String nickname;
+  @JsonKey(name: "is_active")
+  final bool isActive;
+  @JsonKey(name: "is_verified")
+  final bool isVerified;
+  final String role;
+  @JsonKey(name: "created_at")
+  final String createdAt;
+  @JsonKey(name: "updated_at")
+  final String updatedAt;
 
   UserSignUpModel({
-    required this.msg,
+    required this.email,
+    required this.id,
+    required this.nickname,
+    required this.isActive,
+    required this.isVerified,
+    required this.role,
+    required this.createdAt,
+    required this.updatedAt,
   });
 
-  factory UserSignUpModel.fromJson(Map<String, dynamic> json) {
-    return UserSignUpModel(
-      msg: json['msg'] as String,
-    );
-  }
+  factory UserSignUpModel.fromJson(Map<String, dynamic> json) =>
+      _$UserSignUpModelFromJson(json);
 }
