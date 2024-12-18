e7014ca0be1e32fc8d87e5455d10bf2d53f16091
jeonhl7579 <tpgus19@gmail.com>
2024-12-19 01:59:52 +0900

    feat: 전면광고 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  MobileAds.instance.initialize();
 
   runApp(
     EasyLocalization(
